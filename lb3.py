d578a63354b62df23c38039a2aed6c0b266a6e53
rin <arseniu233@gmail.com>
2025-10-05 05:05:12 +0300

    Add files via upload

--- lb3.py
+++ lb3.py
@@ -1,1 +1,12 @@
-print('aaaa')
+'''
+def read_file(mtd):
+        with open('example.txt','r') as file:
+            if mtd=='full':
+                print(file.read())
+            elif mtd=='line':
+                for line in file:
+                    print(line.strip())
+read_file('full')
+print('----')
+read_file('line')
+'''
